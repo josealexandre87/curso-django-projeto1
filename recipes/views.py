5dd67867fcd3b12ac64124dd9382b2142a044bd3
josealexandre87 <jose.josealexandre87@gmail.com>
2022-09-07 14:12:10 -0300

    FONTEAWESOME Foram incluidos no home.html 3 link/css para utilização de font-awesome  (icons) no corpo do html.

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context={
-        'name': 'JOSE ALEXANDRE DA SILVA JUNIOR',
+        'name': 'JOSE ALEXANDRE',
     })
