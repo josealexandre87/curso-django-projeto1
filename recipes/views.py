24e0c9aae2afeafa393b194b16e3fbd93d00513d
josealexandre87 <jose.josealexandre87@gmail.com>
2022-09-07 09:35:46 -0300

    Criação de render Foi inserido à função 'home' o return render com o parâmetro ' contex' que recebe um dicionário.
    Esse dicionário foi exibido no home.html pela notação {{ }}, que é uma espécie de 'print'.
    
    Também foi apresentada a maneira de inserir uma nova pasta templa dentro do arquivo settings.py

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'JOSE ALEXANDRE DA SILVA JUNIOR',
+    })
+
+
+def contato(request):
+    return render(request, 'recipes/contato.html')
 
 
 def sobre(request):
     return HttpResponse('SOBRE')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
